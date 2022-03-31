832dbc217821fa6bedbb660ac0885e5e50a39fe3
Michael Quach <mquac089@uottawa.ca>
2022-03-31 01:21:20 -0400

    Added Edit Patient, linked receptionist pages via buttons

--- clinic/Receptionist/urls.py
+++ clinic/Receptionist/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
 
     path('Add Patient', views.index),
-    path('Set Appointment', views.index2)
-
+    path('Set Appointment', views.index2),
+    path('Edit Patient', views.index3),
+    path('menu', views.index4),
 
 ]
--- clinic/Receptionist/views.py
+++ clinic/Receptionist/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.db import connection
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 from authentication.views import login_required
 
@@ -14,6 +15,79 @@ from django.db.models import Model
 from random import randint
 
 # Create your views here.
+def index4(request):
+    if request.method == "GET":
+         return render (request, "menu.html")
+   
+    return HttpResponse()
+
+def index3(request):
+
+    if request.method == "GET":
+        return render (request, "viewEditPatient.html")
+
+    elif request.method == "POST":
+
+        patID = request.POST["patID"]
+        SSN = request.POST["SSN"]
+        house = request.POST["houseNo"]
+        street = request.POST["street"]
+        city = request.POST["city"]
+        province = request.POST["province"]
+        first = request.POST["fName"]
+        last = request.POST["lName"]
+        gender = request.POST["gender"]
+        email = request.POST["email"]
+        dob = request.POST["dob"]
+        phone = request.POST["phone"]
+
+    if "submit" in request.POST:
+        with connection.cursor() as cursor:
+            #if any are the fields are left empty, the value from the table is updated instead
+            if patID == "":
+                messages.info(request, 'Please enter a patient ID!')
+                #do nothing 
+            else:
+                patID = int(patID)
+                if SSN == "":
+                    cursor.execute('SELECT "SSN_patient" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    SSN = cursor.fetchone()
+                if house == "":
+                    cursor.execute('SELECT "House_no" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    house = cursor.fetchone() 
+                if street == "":
+                    cursor.execute('SELECT "Street" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    street = cursor.fetchone()
+                if city == "":
+                    cursor.execute('SELECT "City_name" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    city = cursor.fetchone()
+                if province == "":
+                    cursor.execute('SELECT "Province" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    province = cursor.fetchone()
+                if first == "":
+                    cursor.execute('SELECT "First_name" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    first = cursor.fetchone()
+                if last == "":
+                    cursor.execute('SELECT "Last_name" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    last = cursor.fetchone()  
+                if gender == "":
+                    cursor.execute('SELECT "Gender" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    gender = cursor.fetchone()
+                if email == "":
+                    cursor.execute('SELECT "Email_patient" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    email = cursor.fetchone()
+                if dob == "":
+                    cursor.execute('SELECT "Dob" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    dob = cursor.fetchone()
+                if phone == "":
+                    cursor.execute('SELECT "Phone_no_patient" FROM "Patient" WHERE "PatientID" = %s',  [patID])
+                    phone = cursor.fetchone()   
+                sql = 'Update "Patient" Set "SSN_patient" = %s, "House_no" = %s, "Street" = %s, "City_name" = %s, "Province" = %s, "First_name" = %s, "Last_name" = %s, "Gender" = %s, "Email_patient" = %s, "Dob" = %s, "Phone_no_patient" = %s WHERE "PatientID" = %s'
+                val = (SSN,house,street,city,province,first,last,gender,email, dob, phone, patID)
+                cursor.execute(sql, val)
+            
+    return HttpResponse()
+
 def index2(request):
 
     if request.method == "GET":
@@ -35,7 +109,7 @@ def index2(request):
         
         with connection.cursor() as cursor:
             cursor.execute('INSERT INTO "Appointment" VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', (0,0,0,day,roomNo,start,end,proType,state,ApptID))
-                   
+    
 
     return HttpResponse()
 
