34d5fa1921177e678d70ea5361f5e5838b68ba73
Lara <laralim10@gmail.com>
2022-03-31 20:19:33 -0400

    added viewing patient records for dentist

--- clinic/dentist/urls.py
+++ clinic/dentist/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dentistHome, name = 'dentistHome'),
     path('view_appt', views.viewAppt, name = 'viewAppointments'),
-    path('view_procedures', views.viewProcedures, name = 'viewProcedures')
+    path('view_procedures', views.viewProcedures, name = 'viewProcedures'),
+    path('view_patient_records', views.viewRecords, name = 'viewRecords')
 ]
