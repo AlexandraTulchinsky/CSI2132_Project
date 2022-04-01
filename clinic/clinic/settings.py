76f88b3893aece6ba258d0c3bbc0d0c1ee2fd8db
Lara <laralim10@gmail.com>
2022-03-31 21:01:47 -0400

    display users (dentists) on homepage

--- clinic/clinic/settings.py
+++ clinic/clinic/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Receptionist',
-    'dentist'
+    'dentist',
+    'home'
 ]
 
 MIDDLEWARE = [
--- clinic/home/views.py
+++ clinic/home/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.db import connection
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
@@ -9,10 +10,17 @@ TEMPLATE_DIRS = (
     'os.path.join(BASE_DIR, "templates"),'
 )
 
-
 def home(request):
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT * FROM "Users" WHERE "User_type" = \'Dentist\' OR "User_type" = \'Hygenist\' OR "User_type" = \'Hygienist\'')
+        dentistRows = dictfetchone(cursor)
 
+    context = {'dentistRows': dentistRows}
+    return render(request, 'home.html', context)
 
-    return HttpResponse( "HOME")
-
-    
+def dictfetchone(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
