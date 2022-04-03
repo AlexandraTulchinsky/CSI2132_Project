01414a87246eab9c1f73f54ed4c584c8563c1c55
Lara <laralim10@gmail.com>
2022-04-03 12:39:32 -0400

    added login requirement to dentist pages

--- clinic/dentist/views.py
+++ clinic/dentist/views.py
@@ -1,9 +1,12 @@
 from django.db import connection
 from django.shortcuts import render
+from authentication.views import login_required
 
+@login_required
 def dentistHome(request):
     return render(request, 'dentistHome.html')
 
+@login_required
 def viewAppt(request):
     with connection.cursor() as cursor:
         cursor.execute('SELECT * FROM "Appointment" WHERE "Status" = \'upcoming\'')
@@ -12,6 +15,7 @@ def viewAppt(request):
     context = {'apptRows': apptRows}
     return render(request, 'viewAppointments.html', context)
 
+@login_required
 def viewProcedures(request):
     with connection.cursor() as cursor:
         cursor.execute('SELECT * FROM "Treatment"')
@@ -25,6 +29,7 @@ def viewProcedures(request):
                
     return render(request, 'viewProcedures.html', context)
 
+@login_required
 def viewRecords(request):
     with connection.cursor() as cursor:
         cursor.execute('SELECT * FROM "Record"')
