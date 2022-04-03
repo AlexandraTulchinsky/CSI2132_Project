04458b5245a2cb55cb61075c13c685a0a1e66a47
Amani Farid <afari094@uottawa.ca>
2022-04-02 22:41:28 -0400

    removed the prints in patient view

--- clinic/patient/views.py
+++ clinic/patient/views.py
@@ -13,36 +13,30 @@ def viewPatient(request):
     with connection.cursor() as cursor:
         cursor.execute('SELECT * FROM "Patient" WHERE "Patient"."PatientID" = 101') 
         patientRows = dictfetchone(cursor) 
-    #print(apptRows)
 
     context = {'patientRows': patientRows[0], 'apptRows':viewAppointments(), 'dentistRow':viewTodayAppoint(), 'recRows':viewPatientRecords}
     
     return render(request, 'patientpage.html', context)
-    #render(apptRows)
 
 def viewAppointments():
     with connection.cursor() as cursor:
         cursor.execute('SELECT * FROM "Appointment" WHERE "Status" = \'upcoming\' AND "PatientID" = 101') 
         
         apptRows = dictfetchone(cursor)
-    #print(apptRows)
-
+  
     context = {'apptRows': apptRows}
     return apptRows
-    #return render(request, 'patientpage.html', context)
 
 def viewTodayAppoint():
     with connection.cursor() as cursor:
         cursor.execute('SELECT * FROM "Appointment","Employee" WHERE "Appointment"."Date" = CURRENT_DATE AND "Appointment"."PatientID" = 0 AND "Appointment"."EID" IN (SELECT "EID" FROM "Employee")')
         dentistRow = dictfetchone(cursor)
-        print(dentistRow)
         return dentistRow[1]
 
 def viewPatientRecords():
     with connection.cursor() as cursor: 
         cursor.execute('SELECT * FROM "Record" WHERE "Record"."PatientID"= 101')
         recRows = dictfetchone(cursor)
-
     
     return recRows
 
